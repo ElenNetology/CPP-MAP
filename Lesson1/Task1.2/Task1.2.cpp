39f8f91917a902c3778c3f35849d8fa40923a6c2
Elen test <lena.prudko@yandex.ru>
2023-12-08 21:57:43 +0400

    Lesson1.2

--- Lesson1/Task1.2/Task1.2.cpp
+++ Lesson1/Task1.2/Task1.2.cpp
@@ -3,26 +3,18 @@
 
 #include <iostream>
 #include <vector>
-#include <random>
-#include <algorithm>
-#include <execution>
 #include <chrono>
 #include <thread>
 #include <Windows.h>
+#include <iomanip>
 
-void Sum1(std::vector<int> v1, std::vector<int> v2, std::vector<int> v3) {
-    std::vector<int> V1(1'000);
-    std::vector<int> V2(1'000);
-    std::vector<int> V3(1'000);
-    std::mt19937 gen;
-    std::uniform_int_distribution<int> dis(0, 1'000);
-    auto rand_num([=]() mutable {return dis(gen); });
-    generate(V1.begin(), V1.end(), rand_num);
-    generate(V2.begin(), V2.end(), rand_num);
+float Sum = 0;
+void Sum1(std::vector<int> V1, std::vector<int> V2) {
     
-    for (int i = 0; i < v1.size(); i++)
+     
+    for (int i = 0; i < V1.size(); i++)
     {
-        v3[i] = v1[i] + v2[i];
+       Sum += V1[i] + V2[i];
     }
    
 
@@ -30,40 +22,52 @@ void Sum1(std::vector<int> v1, std::vector<int> v2, std::vector<int> v3) {
 
 int main ()
 {
-   setlocale(LC_ALL, "Russian");
+   SetConsoleCP(1251);
+   SetConsoleOutputCP(1251);
    std::cout <<"Количество аппаратных ядер "<< std::thread::hardware_concurrency() << std::endl;
    std::vector<int> V1;
    std::vector<int> V2;
-   std::vector<int> V3;
-       
-    const int num_threads = 2;
-   std::thread t[num_threads];
-   auto start1 = std::chrono::steady_clock::now();
-    for (int i = 0; i < num_threads; ++i) {
-        t[i] = std::thread(Sum1, V1, V2, V3);
-     }
-
-    for (int i = 0; i < num_threads; ++i) {
-        t[i].join();
-    }
-    auto end1 = std::chrono::steady_clock::now();
-    std::chrono::duration<double> elapsed1_seconds = end1 - start1;
-    std::cout << "            1000" << std::endl;
-    std::cout << "2 потока = " << elapsed1_seconds.count() << std::endl;
-
-   std::vector<std::thread> ts;
-   auto start = std::chrono::steady_clock::now();
-    for (int i = 0; i < 4; i++) {
-       
-        ts.push_back (std::thread(Sum1, V1, V2, V3));
-}
-    for (auto& th : ts)
-        th.join();
-    auto end = std::chrono::steady_clock::now();
-    std::chrono::duration<double> elapsed_seconds = end - start;
-    std::cout << "4 потока = " << elapsed_seconds.count() << std::endl;
-  
+   std::vector<int> VECSizes{ 1000, 10000, 100000, 1000000 };
+   std::vector<int> CountThreads{ 1, 2, 4, 8, 16 };
    
+   std::cout << std::endl;
+
+   for (const auto& count : CountThreads)
+   {
+       std::cout << count << " потоков\t";
+       for (const auto& VECSize : VECSizes)
+       {
+           V1.resize(VECSize, 1);
+           V2.resize(VECSize, 2);
+           std::vector<std::thread> ts;
+           int VECSize=1;
+           VECSize = VECSize / count;
+           auto start = std::chrono::steady_clock::now();
+           for (int i = 0; i < count; i++)
+           {
+               std::vector<int> newV1;
+               std::vector<int> newV2;
+               int rightBorder = (i != count - 1) ? VECSize * (i + 1) : VECSize;
+               for (int j = VECSize * i; j < rightBorder; ++j)
+               {
+                   newV1.push_back(V1.at(j));
+                   newV2.push_back(V2.at(j));
+               }
+               ts.push_back(std::thread(Sum1, newV1, newV2));
+           }
+           for (auto& V : ts)
+           {
+               V.join();
+           }
+           auto stop = std::chrono::steady_clock::now();
+           auto duration = std::chrono::duration_cast<std::chrono::microseconds> (stop - start);
+           std::cout << static_cast<double>(duration.count()) / 1e6 << "s\t";
+
+           Sum = 0;
+       }
+       std::cout << std::endl;
+
+   }
     
     return 0;
 }
